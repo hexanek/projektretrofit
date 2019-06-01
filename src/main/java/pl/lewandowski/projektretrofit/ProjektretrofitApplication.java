3aa6f2a9f70af6ef6f4801f8bae71982e3eaf4b2
hexanek <hexanek@gmail.com>
2019-06-01 23:02:31 +0200

    Initial commit

---
+++ src/main/java/pl/lewandowski/projektretrofit/ProjektretrofitApplication.java
@@ -0,0 +1,13 @@
+package pl.lewandowski.projektretrofit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjektretrofitApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjektretrofitApplication.class, args);
+    }
+
+}
